24555575b1aa7a1491cb0624efed48d5fdaeddc3
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2022-04-08 11:28:32 -0300

    ⚡️ Add timeout

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-from lib2to3.pgen2.token import ATEQUAL
 import pygame
 from pygame.locals import *
 import paho.mqtt.client as mqtt
@@ -8,38 +7,13 @@ import time
 
 mqttBroker = "labdigi.wiseful.com.br"
 mqttPort = 80
+
 client = mqtt.Client("Python_Client_GrupoB1")
 client.username_pw_set("grupo1-bancadaB1", "L@Bdygy1B1")
 client.connect(mqttBroker, mqttPort)
 
-clientLed1 = mqtt.Client("Python_Client_GrupoB1_Led1")
-clientLed1.username_pw_set("grupo1-bancadaB1", "L@Bdygy1B1")
-clientLed1.connect(mqttBroker, mqttPort)
-
-clientLed2 = mqtt.Client("Python_Client_GrupoB1_Led2")
-clientLed2.username_pw_set("grupo1-bancadaB1", "L@Bdygy1B1")
-clientLed2.connect(mqttBroker, mqttPort)
-
-clientLed3 = mqtt.Client("Python_Client_GrupoB1_Led3")
-clientLed3.username_pw_set("grupo1-bancadaB1", "L@Bdygy1B1")
-clientLed3.connect(mqttBroker, mqttPort)
-
-clientLed4 = mqtt.Client("Python_Client_GrupoB1_Led4")
-clientLed4.username_pw_set("grupo1-bancadaB1", "L@Bdygy1B1")
-clientLed4.connect(mqttBroker, mqttPort)
-
-b0 = "grupo1-bancadaB1/E3"
-b1 = "grupo1-bancadaB1/E4"
-b2 = "grupo1-bancadaB1/E5"
-b3 = "grupo1-bancadaB1/E6"
-
-l0 = "grupo1-bancadaB1/S4"
-l1 = "grupo1-bancadaB1/S5"
-l2 = "grupo1-bancadaB1/S6"
-l3 = "grupo1-bancadaB1/S7"
-
-# username = 'emqx'
-# password = 'public'
+btns_topics = ["grupo1-bancadaB1/E0", "grupo1-bancadaB1/E1", "grupo1-bancadaB1/E2", "grupo1-bancadaB1/E3"]
+leds_topics = ["grupo1-bancadaB1/S0", "grupo1-bancadaB1/S1", "grupo1-bancadaB1/S2", "grupo1-bancadaB1/S3"]
 
 # Game interface configs
 
@@ -54,18 +28,18 @@ screen = pygame.display.set_mode((screenWidth,screenHeight))
 
 logo = pygame.image.load("logoNoBorderTransparent.png")
 strt = pygame.image.load("start.png")
-stat  = pygame.image.load("_stats.png")
+stat  = pygame.image.load("stats.png")
 rst = pygame.image.load("srest.png")
-faixa1 = pygame.image.load("_faixa1.png")
-faixa2 = pygame.image.load("_faixa2.png")
-faixa3 = pygame.image.load("_faixa3.png")
-faixa4 = pygame.image.load("_faixa4.png")
-faixa1play = pygame.image.load("_faixa1play.png")
-faixa2play = pygame.image.load("_faixa2play.png")
-faixa3play = pygame.image.load("_faixa3play.png")
-faixa4play = pygame.image.load("_faixa4play.png")
+faixa1 = pygame.image.load("faixa1.png")
+faixa2 = pygame.image.load("faixa2.png")
+faixa3 = pygame.image.load("faixa3.png")
+faixa4 = pygame.image.load("faixa4.png")
+faixa1play = pygame.image.load("faixa1play.png")
+faixa2play = pygame.image.load("faixa2play.png")
+faixa3play = pygame.image.load("faixa3play.png")
+faixa4play = pygame.image.load("faixa4play.png")
 
-slctTxt = pygame.image.load("_trackSelect.png")
+slctTxt = pygame.image.load("trackSelect.png")
 running = True 
 
 game_font = pygame.font.Font("Minecraft.ttf", 55)
@@ -73,6 +47,13 @@ game_font = pygame.font.Font("Minecraft.ttf", 55)
 totalHits = 0
 totalMisses = 0
 highScore = 0
+led_recieved = 0
+btn_recieved = 0
+misses = 0
+id = 0
+delay = 0
+pontuacao = 0
+acertos = 0
 
 def Stats():
     running = True
@@ -85,6 +66,7 @@ def Stats():
         _hits = "Total hits: " + str(totalHits)
         _misses = "Total misses: " + str(totalMisses)      
         _high = "Highscore: " + str(highScore)
+
         screen.fill((151, 204, 187))
         
         title_render = game_font.render("Estatisticas:", True, (59, 115, 103))
@@ -99,14 +81,8 @@ def Stats():
         high_render = game_font.render(_high, True, (59, 115, 103))
         screen.blit(high_render,((screenWidth-high_render.get_width())/2,100+title_render.get_height()+hits_render.get_height()+misses_render.get_height()+70+70))
 
-        
-
         faixa1btn = pygame.Rect((screenWidth-200)/2,200+title_render.get_height()+hits_render.get_height()+misses_render.get_height()+70+70,200,80)
-        
-
         screen.blit(rst, ((screenWidth-200)/2,200+title_render.get_height()+hits_render.get_height()+misses_render.get_height()+70+70))
-        
-
     
         #Mouse Controller
         mx, my = pygame.mouse.get_pos()
@@ -133,72 +109,83 @@ def Stats():
         pygame.display.update()
         mainClock.tick(60)
 
-led1=0
-led2=0
-led3=0
-led4=0
-iterador = 0
-def decodeLed1(client, userdata, message):
-    global led1
-    anterior = led1
-    led1 = int(message.payload.decode("utf-8"))
-    if anterior == 0 and led1 == 1:
-        iterador += 1
+iterator = 0
+
+def callback(client, userdata, message):
+    global led_recieved
+    global btn_recieved
+    global iterator
+    global misses
+    global totalMisses
+    global id
+    global acertos
+    global totalHits
+    global delay
+    global pontuacao
+
+    print("AAAAAAAAAA")
+
+    for i in range(4):
+        if message.topic == leds_topics[i]:
+            if int(message.payload.decode("utf-8")) == 1:
+                led_recieved = i + 1
+            else:     
+                iterator += 1
+                led_recieved = 0
+                
+                if btn_recieved == 0:
+                    misses += 1
+                    totalMisses += 1
+                    delay = 60 * (6 - id)
+
+            btn_recieved = 0
 
 
+    for i in range(4):
+        if message.topic == btns_topics[i]:
+            if int(message.payload.decode("utf-8")) == 1:
+                if btn_recieved == 0:
+                    btn_recieved = i + 1
+                    if led_recieved == btn_recieved:
+                        if(delay > 0):
+                            pontuacao += 100 * delay // (240 / id)
+                        else:
+                            pontuacao += 1
+                        acertos += 1
+                        totalHits += 1
+                    else:
+                        misses += 1
+                        totalMisses += 1
 
-def decodeLed2(client, userdata, message):
-    global led2
-    anterior = led2
-    led2 = int(message.payload.decode("utf-8"))
-    if anterior == 0 and led2 == 1:
-        iterador += 1
+                    delay = 60 * (6 - id)
 
-def decodeLed3(client, userdata, message):
-    global led3
-    anterior = led3
-    led3 = int(message.payload.decode("utf-8"))
-    if anterior == 0 and led3 == 1:
-        iterador += 1
 
-def decodeLed4(client, userdata, message):
-    global led4
-    anterior = led4
-    led4 = int(message.payload.decode("utf-8"))
-    if anterior == 0 and led4 == 1:
-        iterador += 1
+def Faixa(n):
+    global id
+    global misses
+    global delay
+    global pontuacao
+    global acertos
 
-def Faixa(n, id):
     begin = int(round(time.time()*1000))
     topico = "grupo1-bancadaB1/E" + str(id+2)
     maxnotes = n
     running = True
     click = False
     acertos = 0
-    misses = 0
     delay = 240/id
     pontuacao = 0
+    misses = 0
 
-    global led1
-    global led2
-    global led3
-    global led4
+    client.on_message = callback
 
-    clientLed1.on_message = decodeLed1
-    clientLed1.subscribe("grupo1-bancadaB1/S4")
-    
-    clientLed2.on_message = decodeLed2
-    clientLed2.subscribe("grupo1-bancadaB1/S5")
+    for topic in leds_topics:
+        client.subscribe(topic)
 
-    clientLed3.on_message = decodeLed3
-    clientLed3.subscribe("grupo1-bancadaB1/S6")
+    for topic in btns_topics:
+        client.subscribe(topic)
 
-    clientLed4.on_message = decodeLed4
-    clientLed4.subscribe("grupo1-bancadaB1/S7")
-
-    client.on_message = decodeBit
-    client.subscribe([("grupo1-bancadaB1/E3",0),("grupo1-bancadaB1/E4",0),("grupo1-bancadaB1/E5",0),("grupo1-bancadaB1/E6",0)])
-    global instr
+    global btn_recieved
     global totalHits
     global totalMisses
     global highScore
@@ -206,15 +193,12 @@ def Faixa(n, id):
     while running:
         time.sleep(0.2)
         client.loop_start()
-        clientLed1.loop_start()
-        clientLed2.loop_start()
-        clientLed3.loop_start()
-        clientLed4.loop_start()
+
         if(pontuacao>highScore):
             highScore = pontuacao
 
-        if instr != 0:
-            print(instr)        
+        if btn_recieved != 0:
+            print(btn_recieved)        
     
 
         screen.fill((151, 204, 187))
@@ -254,17 +238,17 @@ def Faixa(n, id):
                 
             if event.type == KEYDOWN:
                 if event.key == K_UP:
-                    if(delay>0):
+                    if(delay > 0):
                         pontuacao += 100 * delay // (240/id)
                     else:
                         pontuacao += 1
                     delay = 240/id
-                    acertos+=1
-                    totalHits+=1
+                    acertos += 1
+                    totalHits += 1
                 if event.key == K_DOWN:
-                    misses+=1
-                    totalMisses+=1
-                    instr = 0
+                    misses += 1
+                    totalMisses += 1
+                    btn_recieved = 0
                     delay = 240/id
 
                 if event.key == K_ESCAPE:
@@ -281,81 +265,35 @@ def Faixa(n, id):
         pygame.display.update()
         mainClock.tick(60)
 
-        if (instr!=0 and acertos + misses >= maxnotes):
+        if (btn_recieved!=0 and acertos + misses >= maxnotes):
             print(topico)
             client.publish(topico, id)
             client.publish(topico, 0)
             pygame.time.delay(300)
             running = False
-        
-        if(instr == 1 and led1):
-            if(delay>0):
-                pontuacao += 100 * delay // (240/id)
-            else:
-                pontuacao += 1
-            delay = 240/id
-            acertos+=1
-            totalHits+=1
-            instr = 0
 
-        elif(instr == 2 and led2):
-            if(delay>0):
-                pontuacao += 100 * delay // (240/id)
-            else:
-                pontuacao += 1
-            delay = 240/id
-            acertos+=1
-            totalHits+=1
-            instr = 0
-        
-        elif(instr == 3 and led3):
-            if(delay>0):
-                pontuacao += 100 * delay // (240/id)
-            else:
-                pontuacao += 1
-            delay = 240/id
-            acertos+=1
-            totalHits+=1
-            instr = 0
-        
-        elif(instr == 4 and led4):
-            if(delay>0):
-                pontuacao += 100 * delay // (240/id)
-            else:
-                pontuacao += 1
-            delay = 240/id
-            acertos+=1
-            totalHits+=1
-            instr = 0
-
-        elif((instr!=0 and not first and (int(round(time.time()*1000)) - begin > 1500))):
-            misses+=1
-            totalMisses+=1
-            instr = 0
-            delay = 240/id
-        print(delay)
+        # print(delay)
         first = False
-        
-        
-instr = 0
-
-def decodeBit(client, userdata, message):
-    global instr
-    instr = int(message.payload.decode("utf-8"))
-    
-     
 
 # Game Screen
 def GameSelect():
-    global instr
-    client.on_message = decodeBit
-    client.subscribe([("grupo1-bancadaB1/E3",0),("grupo1-bancadaB1/E4",0),("grupo1-bancadaB1/E5",0),("grupo1-bancadaB1/E6",0)])
+    global btn_recieved
+    global id
+    
+    client.on_message = callback
+
+    for topic in leds_topics:
+        client.subscribe(topic)
+
+    for topic in btns_topics:
+        client.subscribe(topic)
+
     running = True
     click = False
 
     while running:
-        if instr != 0:
-            print(instr)        
+        if btn_recieved != 0:
+            print(btn_recieved)        
         client.loop_start()
         screen.fill((151, 204, 187))
         screen.blit(slctTxt, ((screenWidth-400)/2,30))
@@ -374,34 +312,38 @@ def GameSelect():
         #Mouse Controller
         mx, my = pygame.mouse.get_pos()
         
-        if faixa1btn.collidepoint((mx, my)) or instr == 1:
-            if click or instr == 1:
-                client.publish(b0, 1)
+        if faixa1btn.collidepoint((mx, my)) or btn_recieved == 1:
+            if click or btn_recieved == 1:
+                client.publish(btns_topics[0], 1)
                 time.sleep(0.1)
-                client.publish(b0, 0)
+                client.publish(btns_topics[0], 0)
                 time.sleep(0.1)
-                Faixa(30, 1)
-        if faixa2btn.collidepoint((mx, my)) or instr == 2:
-            if click or instr == 2:
-                client.publish(b1, 1)
+                id = 1
+                Faixa(30)
+        if faixa2btn.collidepoint((mx, my)) or btn_recieved == 2:
+            if click or btn_recieved == 2:
+                client.publish(btns_topics[1], 1)
                 time.sleep(0.1)
-                client.publish(b1, 0)
+                client.publish(btns_topics[1], 0)
                 time.sleep(0.1)
-                Faixa(30,2)
-        if faixa3btn.collidepoint((mx, my)) or instr == 3:
-            if click or instr == 3:
-                client.publish(b2, 1)
+                id = 2
+                Faixa(30)
+        if faixa3btn.collidepoint((mx, my)) or btn_recieved == 3:
+            if click or btn_recieved == 3:
+                client.publish(btns_topics[2], 1)
                 time.sleep(0.1)
-                client.publish(b2, 0)
+                client.publish(btns_topics[2], 0)
                 time.sleep(0.1)
-                Faixa(30, 3)
-        if faixa4btn.collidepoint((mx, my)) or instr == 4:
-            if click or instr == 4:
-                client.publish(b3, 1)
+                id = 3
+                Faixa(30)
+        if faixa4btn.collidepoint((mx, my)) or btn_recieved == 4:
+            if click or btn_recieved == 4:
+                client.publish(btns_topics[3], 1)
                 time.sleep(0.1)
-                client.publish(b3, 0)
+                client.publish(btns_topics[3], 0)
                 time.sleep(0.1)
-                Faixa(30, 4)
+                id = 4
+                Faixa(30)
 
         click = False
         for event in pygame.event.get():
